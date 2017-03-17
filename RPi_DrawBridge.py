266968f15d48d98a2dbeebf300e9696b7610355c
AlumiNoobie <peter.di.giorgio00@gmail.com>
2017-03-16 22:54:00 -0400

    Version 0.2
    Initialized PWM for two servos.  GPIO12 and GPIO18 on a RPi 3 board will
    be used to control the gates.  Both gate servos will start at the full
    left position which will be gates down.  The plan is to lift one gate
    before entering the polling loop for the button inputs.

--- RPi_DrawBridge.py
+++ RPi_DrawBridge.py
@@ -1,15 +1,30 @@
 #Author:  Peter Di Giorgio
 #Date: 15032017
-#Version: 0.1
+#Version: 0.2
 #Purpose: Drawbridge script for BSidesAugusta STEM.
 
 
 import RPi.GPIO as GPIO
 import time
 
+#Set the GPIO mode for BCM numbering
 GPIO.setmode(GPIO.BCM)
 
 #Input Pins
+
 #Output Pins
+
 #Output PWM: GPIO12, GPIO18
-GPIO.setup(
+servoPIN12 = 12
+servoPIN18 = 18
+GPIO.setup(servoPIN12,GPIO.OUT)
+GPIO.setup(servoPIN18,GPIO.OUT)
+
+#Set 50 Hz
+RoadWay_Gate = GPIO.PWM(servoPIN12,50)
+WaterWay_Gate = GPIO.PWM(servoPIN18,50)
+
+#Start servos at full left position or both gates down
+RoadWay_Gate.start(5)
+WaterWay_Gate.start(5)
+
