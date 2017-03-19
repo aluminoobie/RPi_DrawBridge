62735264a43c692373fe8cfb0b23193d480df0b7
AlumiNoobie <aluminoobie@users.noreply.github.com>
2017-03-19 04:28:33 -0400

    Major Update
    Programmed all inputs and outputs.  Created main while loop to read push button inputs.  Ready for testing.

--- RPi_DrawBridge.py
+++ RPi_DrawBridge.py
@@ -1,6 +1,6 @@
 #Author:  Peter Di Giorgio
-#Date: 15032017
-#Version: 0.2
+#Date: 19032017
+#Version: 0.3
 #Purpose: Drawbridge script for BSidesAugusta STEM.
 
 
@@ -9,10 +9,21 @@ import time
 
 #Set the GPIO mode for BCM numbering
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 #Input Pins
+PSHBTNPIN17 = 17
+PSHBTNPIN27 = 27
+GPIO.setup(PSHBTNPIN17,GPIO.IN,GPIO.PUD_UP)
+GPIO.setup(PSHBTNPIN27,GPIO.IN,GPIO.PUD.UP)
 
 #Output Pins
+LEDPIN16 = 16
+LEDPIN20 = 20
+LEDPIN21 = 21
+GPIO.setup(LEDPIN16,GPIO.OUT)
+GPIO.setup(LEDPIN20,GPIO.OUT)
+GPIO.setup(LEDPIN21,GPIO.OUT)
 
 #Output PWM: GPIO12, GPIO18
 servoPIN12 = 12
@@ -28,3 +39,49 @@ WaterWay_Gate = GPIO.PWM(servoPIN18,50)
 RoadWay_Gate.start(5)
 WaterWay_Gate.start(5)
 
+#Function to change lights
+def ChgLights (Open_Road):
+  if Open_Road = 'RoadWay':
+    GPIO.output(LEDPIN16,LOW)
+    GPIO.output(LEDPIN20,HIGH)
+    time.sleep(1)
+    GPIO.output(LEDPIN20,LOW)
+    GPIO.output(LEDPIN21,HIGH)
+    return;
+  if Open_Road = 'WaterWay':
+    GPIO.output(LEDPIN21,LOW)
+    GPIO.output(LEDPIN20,HIGH)
+    time.sleep(1)
+    GPIO.output(LEDPIN20,LOW)
+    GPIO.output(LEDPIN16,HIGH)
+    return;
+  
+while True:
+  Road_Input = GPIO.input(PSHBTNPIN17)
+  Water_Input = GPIO.input(PSHBTNPIN27)
+  if Road_Input = True and Water_Input  = False:
+    Open_Road = 'RoadWay'
+    ChgLights(Open_Road)
+    RoadWay_Gate.start(10)
+    WaterWay_Gate.start(5)
+  if Road_Input = False and Water_Input = True:
+    Open_Road = 'WaterWay'
+    ChgLights(Open_Road)
+    RoadWay_Gate.start(5)
+    WaterWay_Gate.start(10)
+  if Road_Input = True and Water_Input = True:
+    RoadWay_Gate.start(5)
+    WaterWay_Gate.start(5)
+    GPIO.output(LEDPIN16,HIGH)
+    time.sleep(0.5)
+    GPIO.output(LEDPIN20,HIGH)
+    time.sleep(0.5)
+    GPIO.output(LEDPIN21,HIGH)
+    GPIO.output(LEDPIN16,LOW)
+    GPIO.output(LEDPIN20,LOW)
+    GPIO.output(LEDPIN21,LOW
+    break
+  time.sleep(0.1)
+
+print "Good bye!"
+GPIO.cleanup()
