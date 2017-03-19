23aedffb00a10b72447937736193aae30902d11c
AlumiNoobie <aluminoobie@users.noreply.github.com>
2017-03-19 09:20:54 +0000

    First Test and Debug
    Tested script and debugged.

--- RPi_DrawBridge.py
+++ RPi_DrawBridge.py
@@ -15,7 +15,7 @@ GPIO.setwarnings(False)
 PSHBTNPIN17 = 17
 PSHBTNPIN27 = 27
 GPIO.setup(PSHBTNPIN17,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(PSHBTNPIN27,GPIO.IN,GPIO.PUD.UP)
+GPIO.setup(PSHBTNPIN27,GPIO.IN,GPIO.PUD_UP)
 
 #Output Pins
 LEDPIN16 = 16
@@ -41,45 +41,46 @@ WaterWay_Gate.start(5)
 
 #Function to change lights
 def ChgLights (Open_Road):
-  if Open_Road = 'RoadWay':
-    GPIO.output(LEDPIN16,LOW)
-    GPIO.output(LEDPIN20,HIGH)
+  if Open_Road == 'RoadWay':
+    GPIO.output(LEDPIN16,GPIO.LOW)
+    GPIO.output(LEDPIN20,GPIO.HIGH)
     time.sleep(1)
-    GPIO.output(LEDPIN20,LOW)
-    GPIO.output(LEDPIN21,HIGH)
+    GPIO.output(LEDPIN20,GPIO.LOW)
+    GPIO.output(LEDPIN21,GPIO.HIGH)
     return;
-  if Open_Road = 'WaterWay':
-    GPIO.output(LEDPIN21,LOW)
-    GPIO.output(LEDPIN20,HIGH)
+  if Open_Road == 'WaterWay':
+    GPIO.output(LEDPIN21,GPIO.LOW)
+    GPIO.output(LEDPIN20,GPIO.HIGH)
     time.sleep(1)
-    GPIO.output(LEDPIN20,LOW)
-    GPIO.output(LEDPIN16,HIGH)
+    GPIO.output(LEDPIN20,GPIO.LOW)
+    GPIO.output(LEDPIN16,GPIO.HIGH)
     return;
   
 while True:
   Road_Input = GPIO.input(PSHBTNPIN17)
   Water_Input = GPIO.input(PSHBTNPIN27)
-  if Road_Input = True and Water_Input  = False:
+  if Road_Input == True and Water_Input  == False:
     Open_Road = 'RoadWay'
     ChgLights(Open_Road)
     RoadWay_Gate.start(10)
     WaterWay_Gate.start(5)
-  if Road_Input = False and Water_Input = True:
+  if Road_Input == False and Water_Input == True:
     Open_Road = 'WaterWay'
     ChgLights(Open_Road)
     RoadWay_Gate.start(5)
     WaterWay_Gate.start(10)
-  if Road_Input = True and Water_Input = True:
+  if Road_Input == True and Water_Input == True:
     RoadWay_Gate.start(5)
     WaterWay_Gate.start(5)
-    GPIO.output(LEDPIN16,HIGH)
+    GPIO.output(LEDPIN16,GPIO.HIGH)
     time.sleep(0.5)
-    GPIO.output(LEDPIN20,HIGH)
+    GPIO.output(LEDPIN20,GPIO.HIGH)
     time.sleep(0.5)
-    GPIO.output(LEDPIN21,HIGH)
-    GPIO.output(LEDPIN16,LOW)
-    GPIO.output(LEDPIN20,LOW)
-    GPIO.output(LEDPIN21,LOW
+    GPIO.output(LEDPIN21,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(LEDPIN16,GPIO.LOW)
+    GPIO.output(LEDPIN20,GPIO.LOW)
+    GPIO.output(LEDPIN21,GPIO.LOW)
     break
   time.sleep(0.1)
 
