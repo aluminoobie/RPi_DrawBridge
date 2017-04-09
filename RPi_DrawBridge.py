e358b729a3f6b491f9108c7bd2e8fb31cb17bfdb
AlumiNoobie <aluminoobie@users.noreply.github.com>
2017-04-09 17:09:24 +0000

    RPi_DrawBridge v1.0
    This version has been tested with hardware.  It functions with included circuit diagram (BSidesAugustaSTEM_RPi_DrawBridge.pdf)

--- RPi_DrawBridge.py
+++ RPi_DrawBridge.py
@@ -1,6 +1,6 @@
 #Author:  Peter Di Giorgio
-#Date: 19032017
-#Version: 0.3
+#Date: 09042017
+#Version: 1.0
 #Purpose: Drawbridge script for BSidesAugusta STEM.
 
 
@@ -14,8 +14,8 @@ GPIO.setwarnings(False)
 #Input Pins
 PSHBTNPIN17 = 17
 PSHBTNPIN27 = 27
-GPIO.setup(PSHBTNPIN17,GPIO.IN,GPIO.PUD_UP)
-GPIO.setup(PSHBTNPIN27,GPIO.IN,GPIO.PUD_UP)
+GPIO.setup(PSHBTNPIN17,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PSHBTNPIN27,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 #Output Pins
 LEDPIN16 = 16
@@ -26,14 +26,14 @@ GPIO.setup(LEDPIN20,GPIO.OUT)
 GPIO.setup(LEDPIN21,GPIO.OUT)
 
 #Output PWM: GPIO12, GPIO18
-servoPIN12 = 12
-servoPIN18 = 18
-GPIO.setup(servoPIN12,GPIO.OUT)
-GPIO.setup(servoPIN18,GPIO.OUT)
+servoPINRW = 12
+servoPINWW = 13
+GPIO.setup(servoPINRW,GPIO.OUT)
+GPIO.setup(servoPINWW,GPIO.OUT)
 
 #Set 50 Hz
-RoadWay_Gate = GPIO.PWM(servoPIN12,50)
-WaterWay_Gate = GPIO.PWM(servoPIN18,50)
+RoadWay_Gate = GPIO.PWM(servoPINRW,50)
+WaterWay_Gate = GPIO.PWM(servoPINWW,50)
 
 #Start servos at full left position or both gates down
 RoadWay_Gate.start(5)
@@ -47,6 +47,7 @@ def ChgLights (Open_Road):
     time.sleep(1)
     GPIO.output(LEDPIN20,GPIO.LOW)
     GPIO.output(LEDPIN21,GPIO.HIGH)
+    print "Road Way is Open and Water Way is Closed"
     return;
   if Open_Road == 'WaterWay':
     GPIO.output(LEDPIN21,GPIO.LOW)
@@ -54,22 +55,26 @@ def ChgLights (Open_Road):
     time.sleep(1)
     GPIO.output(LEDPIN20,GPIO.LOW)
     GPIO.output(LEDPIN16,GPIO.HIGH)
+    print "Road Way is Closed and Water Way is Open"
     return;
   
 while True:
   Road_Input = GPIO.input(PSHBTNPIN17)
   Water_Input = GPIO.input(PSHBTNPIN27)
   if Road_Input == True and Water_Input  == False:
+    print "Opening Road Way"
     Open_Road = 'RoadWay'
     ChgLights(Open_Road)
     RoadWay_Gate.start(10)
     WaterWay_Gate.start(5)
   if Road_Input == False and Water_Input == True:
+    print "Opening Water Way"
     Open_Road = 'WaterWay'
     ChgLights(Open_Road)
     RoadWay_Gate.start(5)
     WaterWay_Gate.start(10)
   if Road_Input == True and Water_Input == True:
+    print "Closing Road and Water Way.  Shutting System Down...."
     RoadWay_Gate.start(5)
     WaterWay_Gate.start(5)
     GPIO.output(LEDPIN16,GPIO.HIGH)
